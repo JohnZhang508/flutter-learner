66f3335cc39fff6e5390e122773d4dc4f4f041be
JohnZhangZzz <john.zhang@mountain-pass.com.au>
2023-03-02 15:47:24 +1100

    add world

--- my_casual_game/lib/ray_world_game.dart
+++ my_casual_game/lib/ray_world_game.dart
@@ -1,9 +1,12 @@
 import 'package:flame/game.dart';
 import 'components/player.dart';
 import '../helpers/direction.dart';
+import 'components/world.dart';
+import 'dart:ui';
 
 class RayWorldGame extends FlameGame {
   final Player _player = Player();
+  final World _world = World();
 
   void onJoypadDirectionChanged(Direction direction) {
     _player.direction = direction;
@@ -11,6 +14,10 @@ class RayWorldGame extends FlameGame {
 
   @override
   Future<void> onLoad() async {
+    await add(_world);
     add(_player);
+    _player.position = _world.size / 2;
+    camera.followComponent(_player,
+        worldBounds: Rect.fromLTRB(0, 0, _world.size.x, _world.size.y));
   }
 }
